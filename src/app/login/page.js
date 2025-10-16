e3dab5ae61dddeee82b60ab5d5ba480926462c8f
Proctor <a.@Proctors-MacBook-Pro.local>
2025-10-16 01:52:45 -0700

    sxInitial commit - Next.js accounting website

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -41,6 +41,8 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen font-sans flex flex-col">
       {/* NAVBAR */}
+              <title>login</title>
+
       <header className="bg-white border-b">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-20">
